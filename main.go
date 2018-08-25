c10e4eba356bcd868e4929b084a1e5195fc96c9d
Jihui Choi <jihui.choi@strix.co.kr>
2018-08-25 14:38:14 +0900

    building an html and javascript chat client

--- main.go
+++ main.go
@@ -24,8 +24,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// root
+	r := newRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 
 	// start the web setver
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- room.go
+++ room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
@@ -18,19 +24,53 @@ type room struct {
 func (r *room) run() {
 	for {
 		select {
-		case client := <- r.join:
+		case client := <-r.join:
 			// joining
 			r.clients[client] = true
-		case client := <- r.leave:
+		case client := <-r.leave:
 			// leaving
 			delete(r.clients, client)
 			close(client.send)
-		case msg := <- r.forward:
+		case msg := <-r.forward:
 			// forward message to all clients
 			for client := range r.clients {
 				client.send <- msg
 			}
 
 		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil) // http 연결을 웹소켓으로 변경
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client                     // 클라이언트가 룸에 조인
+	defer func() { r.leave <- client }() // 웹소켓 종료시 클라이언트가 룸에서 떠남을 기록
+	go client.write()                    // 클라이언트 화면에 메세지를 뿌림
+	client.read()                        // 클라이언트의 입력을 기다림
+}
+
+// room struct instance 를 만들어서 그 주소에 있는 값을 돌려준다.
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
 	}
 }
